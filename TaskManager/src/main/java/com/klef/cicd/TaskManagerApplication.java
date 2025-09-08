563560aa8ce4660d78cd5a8d7a12f1a1ec5a6e89
Haswanth Chinni <163897416+haswanth04@users.noreply.github.com>
2025-09-09 00:28:07 +0530

    Update TaskManagerApplication.java

--- TaskManager/src/main/java/com/klef/cicd/TaskManagerApplication.java
+++ TaskManager/src/main/java/com/klef/cicd/TaskManagerApplication.java
@@ -2,6 +2,7 @@ package com.klef.cicd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class TaskManagerApplication extends SpringBootServletInitializer
